336e6f4230acc6815ba09b49a7fff9de1380a6cb
Lloyd Canis <lloyd.canis@gmail.com>
2020-10-12 15:30:15 -0400

    Remove Logging

--- server.js
+++ server.js
@@ -11,20 +11,6 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-    var now = new Date().toString();
-    var log = (`${now}: ${req.method} ${req.url}`);
-    console.log(log);
-
-    fs.appendFile('server.log', log + '\n', (err) => {
-        if (err) {
-            console.log('Unable to append to server.log.')
-        }
-    });
-
-    next();
-});
-
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
